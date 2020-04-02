bbfc6d63e41ed721dcc7fa7a229e50e5ff3122ea
hanameee <hanameee@gmail.com>
2020-04-03 02:07:07 +0900

    Update baekjoon - 10. 그래프 기본 탐색 알고리즘 (1)

--- Fastcampus/baekjoon/src/1697.py
+++ Fastcampus/baekjoon/src/1697.py
@@ -1,25 +1,21 @@
-from collections import deque
 import sys
+from collections import deque
 
-MAX = 100001
-array = [0] * MAX
 n, k = map(int, sys.stdin.readline().split())
+MAX = 100001
+depth = [0 for _ in range(MAX)]
+queue = deque([n])
 
 
-def bfs():
-    q = deque([n])
-    while q:
-        now_pos = q.popleft()
-        if now_pos == k:
-            return array[now_pos]
-        # 이동할 수 있는 거리가 3가지 뿐이기에, 3가지 경우로 다음 정점들을 해결할 수 있음
-        for next_pos in (now_pos-1, now_pos+1, now_pos*2):
-            # 범위에 포함되어 있고 방문하지 않았다면
-            if 0 <= next_pos < MAX and not array[next_pos]:
-                array[next_pos] = array[now_pos] + 1
-                # 최소시간에 대한 정보를 array에 담아주고
-                # 다시 q에 넣어줘서 반복적으로 dfs 수행
-                q.append(next_pos)
+def dfs():
+    while queue:
+        current_node = queue.popleft()
+        if current_node == k:
+            return depth[current_node]
+        for next_node in [current_node-1, current_node+1, current_node*2]:
+            if 0 <= next_node < MAX and depth[next_node] == 0:
+                queue.append(next_node)
+                depth[next_node] = depth[current_node] + 1
 
 
-print(bfs())
+print(dfs())
