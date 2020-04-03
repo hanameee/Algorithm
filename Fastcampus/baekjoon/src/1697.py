dab73da9bb46d62c0fd203d081a6dfab1706d53c
hanameee <hanameee@gmail.com>
2020-04-03 17:06:51 +0900

    Update baekjoon - 1697.숨바꼭질 다른 풀이이

--- Fastcampus/baekjoon/src/1697.py
+++ Fastcampus/baekjoon/src/1697.py
@@ -1,21 +1,23 @@
 import sys
 from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-MAX = 100001
-depth = [0 for _ in range(MAX)]
-queue = deque([n])
+
+def bfs():
+    n, k = map(int, sys.stdin.readline().split())
+    limit = 100000
+    visited = [0 for _ in range(limit+1)]
+    queue = deque([n])
+    time = 0
+    while True:
+        for i in range(len(queue)):
+            current_node = queue.popleft()
+            if current_node == k:
+                return time
+            for next_node in [current_node-1, current_node+1, current_node*2]:
+                if 0 <= next_node <= limit and not visited[next_node]:
+                    queue.append(next_node)
+                    visited[next_node] = True
+        time += 1
 
 
-def dfs():
-    while queue:
-        current_node = queue.popleft()
-        if current_node == k:
-            return depth[current_node]
-        for next_node in [current_node-1, current_node+1, current_node*2]:
-            if 0 <= next_node < MAX and depth[next_node] == 0:
-                queue.append(next_node)
-                depth[next_node] = depth[current_node] + 1
-
-
-print(dfs())
+print(bfs())
