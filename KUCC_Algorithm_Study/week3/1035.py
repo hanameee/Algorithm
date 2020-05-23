31c39b53bee7e6d531f5c9dbf3df252f42eacfa6
hanameee <hanameee@gmail.com>
2020-05-23 18:43:12 +0900

    Update KUCC Algorithm study - week3

--- KUCC_Algorithm_Study/week3/1035.py
+++ KUCC_Algorithm_Study/week3/1035.py
@@ -1,56 +1,41 @@
-from collections import deque
+from itertools import combinations
+directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def dfs(curr, target):
+    x, y = arr[node_idx]
+    for dx, dy in directions:
+        nx = dx+x
+        ny = dy+y
+
+
+def is_path(combination):
+    arr = list(map(map_position, combination))
+    visited = [0]*len(arr)
+    for node_idx in len(arr):
+        if not visited[node_idx]:
+            x, y = arr[node_idx]
+            for dx, dy in directions:
+                nx = dx+x
+                ny = dy+y
+    pass
+
+
+def map_position(num):
+    x = num//5
+    y = num % 5
+    return [x, y]
 
 
 board = [list(input().strip()) for i in range(5)]
-candidate_list = []
-directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-
-def calculate_distance(a, b):
-    return abs(a[0]-b[0])+abs(a[1]-b[1])
-
-
-def bfs(need_visit, target, visited):
-    global board, directions
-    while need_visit:
-        x, y, path = need_visit.popleft()
-        if [x, y] in target:
-            print(path)
-            if len(path) < 2:
-                return [0, [x, y]]
-            return [len(path)-2, path[-2]]
-        for dx, dy in directions:
-            nx = dx+x
-            ny = dy+y
-            if nx < 0 or ny < 0 or nx > 4 or ny > 4 or [nx, ny] in path:
-                continue
-            need_visit.append([nx, ny, path+[[nx, ny]]])
-
+parts_num = 0
 
 for i in range(5):
     for j in range(5):
         if board[i][j] == "*":
-            candidate_list.append([i, j])
+            parts_num += 1
 
-distances = []
-for i in range(len(candidate_list)):
-    distance = 0
-    for j in range(len(candidate_list)):
-        if j != i:
-            distance += calculate_distance(
-                candidate_list[i], candidate_list[j])
-    candidate_list[i].append(distance)
-
-candidate_list.sort(key=lambda x: x[2])
-target = [[candidate_list[0][0], candidate_list[0][1]]]
-total_length = 0
-
-for candidate in candidate_list[1:]:
-    need_visit = deque(
-        [[candidate[0], candidate[1], [[candidate[0], candidate[1]]]]])
-    visited = [[0]*5 for i in range(5)]
-    length, last_position = bfs(need_visit, target, visited)
-    total_length += length
-    target.append([last_position[0], last_position[1]])
-
-print(total_length)
+position_combination = combinations(range(25), parts_num)
+for combination in position_combination:
+    if is_path(combination):
+        print(combination)
--- KUCC_Algorithm_Study/week3/test.py
+++ KUCC_Algorithm_Study/week3/test.py
@@ -1,3 +1,20 @@
-for _ in range(9):
-    arr = input().split()
-    print("".join(arr))
+import math
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+logged_num = int(math.log2(n))
+answer = 3**logged_num
+n -= 2**logged_num
+while n > 1:
+    logged_num = int(math.log2(n))
+    cumm = 3**logged_num
+    answer += cumm
+    n -= 2**logged_num
+if n == 0:
+    return answer
+else:
+    return answer + 1
+
+print(solution(n))
