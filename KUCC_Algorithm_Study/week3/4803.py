1ac2c78e57562bdafee074ac5051823503cb09bd
hanameee <hanameee@gmail.com>
2020-06-19 22:29:38 +0900

    Update KUCC Algorithm study - week3

--- KUCC_Algorithm_Study/week3/4803.py
+++ KUCC_Algorithm_Study/week3/4803.py
@@ -1,23 +1,20 @@
-from collections import deque
+parent = [0 for i in range(501)]
+cycle = [0 for i in range(501)]
 
 
-def bfs(node):
-    global trees, visited, adj
-    visited[node] = 1
-    v_ = 1
-    e_ = 0
-    need_visit = deque([node])
-    while need_visit:
-        current_node = need_visit.popleft()
-        for adj_node in adj[current_node]:
-            e_ += 1
-            if not visited[adj_node]:
-                visited[adj_node] = 1
-                v_ += 1
-                need_visit.append(adj_node)
-    if v_ == e_+1:
-        trees += 1
+def union(a, b):
+    if cycle[a] or cycle[b]:
+        cycle[a] = 1
+        cycle[b] = 1
+    parent[b] = a
     return
+
+
+def find_parent(node):
+    global parent
+    if parent[node] != node:
+        parent[node] = find_parent(parent[node])
+    return parent[node]
 
 
 case = 1
@@ -25,19 +22,23 @@ while True:
     n, m = map(int, input().split())
     if n == 0 and m == 0:
         break
-    adj = [[] for i in range(n+1)]
+    for i in range(1, n+1):
+        parent[i] = i
+        cycle[i] = 0
     for _ in range(m):
         a, b = map(int, input().split())
-        x = min(a, b)
-        y = max(a, b)
-        if x == y:
-            continue
-        adj[x].append(y)
-    visited = [0 for i in range(n+1)]
+        a_parent = find_parent(a)
+        b_parent = find_parent(b)
+        if a_parent == b_parent:
+            cycle[a_parent] = 1
+        else:
+            union(a_parent, b_parent)
     trees = 0
-    for i in range(1, n+1):
-        if not visited[i]:
-            bfs(i)
+    for idx in range(1, len(parent)):
+        p = find_parent(parent[idx])
+        if p == idx and not cycle[p]:
+            trees += 1
+        parent[idx] = p
     if trees == 0:
         print("Case {0}: No trees.".format(case))
     elif trees == 1:
