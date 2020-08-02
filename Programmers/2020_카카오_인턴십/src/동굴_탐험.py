302c16951c5e6015bbafd2f7517319a327302720
hanameee <hanameee@gmail.com>
2020-08-02 23:59:22 +0900

    [Programmers] Update 2020 카카오 인턴십 (동굴 탐험) - 풀이 추가

--- Programmers/2020_카카오_인턴십/src/동굴_탐험.py
+++ Programmers/2020_카카오_인턴십/src/동굴_탐험.py
@@ -1,62 +1,54 @@
 # _*_ coding: utf-8 _*_
-import sys
 from collections import deque
-sys.setrecursionlimit(10**9)
-
-
-def set_adj_graph(path):
-    adj_graph = [[] for _ in range(N)]
-    for node_a, node_b in path:
-        adj_graph[node_a].append(node_b)
-        adj_graph[node_b].append(node_a)
-    return adj_graph
-
-
-def set_dir_graph(graph, order):
-    dir_graph = [[] for _ in range(N)]
-    # bfs로 방향 그래프 만들기
-    need_visit = deque([0])
-    visited = [0 for _ in range(N)]
-    while need_visit:
-        curr_node = need_visit.popleft()
-        visited[curr_node] = 1
-        for adj_node in graph[curr_node]:
-            if not visited[adj_node]:
-                dir_graph[curr_node].append(adj_node)
-                need_visit.append(adj_node)
-    # order도 추가해주기
-    for prev_node, next_node in order:
-        dir_graph[prev_node].append(next_node)
-    return dir_graph
-
-
-def is_cyclic(start_node):
-    visited[start_node] = True
-    in_dfs[start_node] = True
-    for child_node in dir_graph[start_node]:
-        if visited[child_node]:
-            if in_dfs[child_node]:
-                return True
-            continue
-        if is_cyclic(child_node):
-            return True
-    in_dfs[start_node] = False
-    return False
 
 
 def solution(n, path, order):
-    global N, adj_graph, dir_graph, visited, in_dfs
-    N = n
-    adj_graph = set_adj_graph(path)
-    dir_graph = set_dir_graph(adj_graph, order)
-    visited = [0 for _ in range(N)]
-    in_dfs = [0 for _ in range(N)]
-    visited[0] = 1
-    flag = is_cyclic(0)
-    return False if flag else True
+    need_visit = deque()
+    answer = False
+    visit = [0]*n
+    visit[0] = 1
+    cango = [0]*n
+    cango[0] = 1
+    parent_graph = {}
+    child_graph = {}
+    for prev_node, next_node in order:
+        parent_graph[next_node] = prev_node
+        child_graph[prev_node] = next_node
+    graph = [[] for _ in range(n)]
+    # graph 완성해주기 (양방향 그래프)
+    for prev_node, next_node in path:
+        graph[prev_node].append(next_node)
+        graph[next_node].append(prev_node)
+    # root node인 0이 선행지점이 없는지 체크해야 함
+    if parent_graph.get(0) is None:
+        need_visit.extend(graph[0])
+    while need_visit:
+        curr_node = need_visit.popleft()
+        cango[curr_node] = 1
+        # curr_node를 방문하기 위한 선행지점이 없을 경우
+        if parent_graph.get(curr_node) is None:
+            visit[curr_node] = 1
+            for adj_node in graph[curr_node]:
+                if visit[adj_node] == 0:
+                    need_visit.append(adj_node)
+            # curr_node을 선행지점으로 하는 노드가 있고, 해당 노드를 갈 수 있는 경우
+            if child_graph.get(curr_node) is not None and cango[child_graph[curr_node]]:
+                need_visit.append(child_graph[curr_node])
+        else:
+            # curr_node를 방문하기 위한 선행지점이 있는데, 그 선행 지점을 방문했을 경우
+            if visit[parent_graph.get(curr_node)] == 1:
+                visit[curr_node] = 1
+                for adj_node in graph[curr_node]:
+                    if visit[adj_node] == 0:
+                        need_visit.append(adj_node)
+    if sum(visit) == n:
+        answer = True
+    return answer
 
 
 print(solution(9, [[0, 1], [0, 3], [0, 7], [8, 1], [3, 6],
                    [1, 2], [4, 7], [7, 5]], [[8, 5], [6, 7], [4, 1]]))
 print(solution(9, [[0, 1], [0, 3], [0, 7], [8, 1], [3, 6],
                    [1, 2], [4, 7], [7, 5]], [[4, 1], [8, 7], [6, 5]]))
+
+# 0,3,4,6,7
