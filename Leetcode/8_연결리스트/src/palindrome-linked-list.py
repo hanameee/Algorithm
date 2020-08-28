9c209095c77fdb093f9f9757806c181f8be06881
hanameee <hanameee@gmail.com>
2020-08-28 23:46:58 +0900

    [Leetcode] Update 8.연결리스트

---
+++ Leetcode/8_연결리스트/src/palindrome-linked-list.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+def solution(head):
+    q = deque([])
+    if not head:
+        return True
+    node = head
+    while node is not None:
+        q.append(node.val)
+        q = node.next
+    while len(q) > 1:
+        if q.pop() != q[-1]:
+            return False
+
+
+solution()
