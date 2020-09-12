ae7df386d0f98f58f8d9597598b35080a71f14e0
hanameee <hanameee@gmail.com>
2020-09-12 23:37:51 +0900

    [Programmers] Update 연습문제 - 거스름돈

--- Programmers/연습문제/src/거스름돈.py
+++ Programmers/연습문제/src/거스름돈.py
@@ -1,17 +1,19 @@
 def solution(n, money):
-    money = [0]+money
+    money = money
     dp = [[0]*(n+1) for _ in range(len(money))]
     for i in range(len(money)):
         dp[i][0] = 1
-    for i in range(money[1], n+1, money[1]):
-        dp[1][i] = 1
-    for i in range(2, len(money)):
+    for i in range(money[0], n+1, money[0]):
+        dp[0][i] = 1
+    for i in range(1, len(money)):
         for j in range(n+1):
             if money[i] > j:
                 dp[i][j] = dp[i-1][j]
             if money[i] <= j:
                 dp[i][j] = (dp[i-1][j]+dp[i][j-money[i]]) % 1000000007
-    return
+    for _ in dp:
+        print(_)
+    return dp[-1][-1]
 
 
-solution(5, [1, 2, 5])
+print(solution(5, [1, 2, 5]))
